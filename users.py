fcfde4fb456dd73beccba03f5dd287d60ed8e520
arfinaleusuf <arfinaleusuf@gmail.com>
2026-05-02 01:02:54 +0600

    refactor: move Restaurent class to restaurant.py for better organization

--- users.py
+++ users.py
@@ -86,21 +86,6 @@ class Order:
         self.items = {}
 
 
-class Restaurent:
-    def __init__(self, name):
-        self.name = name
-        self.employees = [] # eta hocche amader database
-        self.menu = Menu()
-    
-    def add_employee(self, employee):
-        self.employees.append(employee)
-
-    def view_employee(self):
-        print("Employee List!!")
-        for emp in self.employees:
-            print(emp.name, emp.email, emp.phone, emp.address)
-
-
 class Menu:
     def __init__(self):
         self.items = [] #items er database
